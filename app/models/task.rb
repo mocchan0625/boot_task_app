44c35599e07abb3b0405fdd0f19678c173a2b8f1
mocchan0625 <yuya.mottate0625@gmail.com>
2015-02-18 23:16:58 +0900

    bootstarp

---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ActiveRecord::Base
+  belongs_to :project
+  validates :title,
+  presence: { message: "入力してください" }
+  scope :unfinished, -> { where(done: false) }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root to: "projects#index"
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
